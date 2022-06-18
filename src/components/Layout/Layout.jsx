f1242d8ceef29d2aee5c753bf0c292c3a6b2cce0
sohseyedi <sohseyedi@gmail.com>
2022-06-18 11:42:52 +0430

    slider create

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Container from './../../container/Container';
 import Header from './Header/Header';
+import Slider from './Slider/Slider';
 
 const Layout = () => {
   return (
     <Container>
         <Header/>
+        <Slider/>
     </Container>
   )
 }
